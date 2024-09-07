bf0800a99bb8e7fcfde05f19b58050c6ece523b8
johnsharrar <js1025519@wcupa.edu>
2024-09-07 18:28:49 -0400

    Completed Week 2 assignments

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -4,11 +4,23 @@ public class Q1WeirdFunction {
 
     public static int fRecursive(int n) {
         // TODO: Implement the recursive function
-        return 0;
+        if(n >= 3){
+            return fRecursive(n-1) + fRecursive(n-2) + fRecursive(n-3);
+        }
+        return n;
     }
 
     public static int fIterative(int n) {
         // TODO: Implement the iterative function
-        return 0;
+        int a = 0;
+        int b = 1;
+        int c = 2;
+        for(int i = 3; i <= n; i++){
+            int temp = a + b + c;
+            a = b;
+            b = c;
+            c = temp;
+        }
+        return c;
     }
 }
